bb9511abb1d2fb9a9225ac9ab66e04b05a182f87
jiuraque <jiulianna.raquel@gmail.com>
2025-03-20 14:58:32 -0300

    se definen las variables a utilizar y la api

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const api = "https://mindicador.cl/api/"
+
+let monto = document.getElementById("monto");
+let monedaSelect = document.getElementById("moneda");
+let calculoBoton = document.getElementById("calculo");
+let resultado = document.getElementById("resultado");
+let items = document.getElementById("items");
+
+
