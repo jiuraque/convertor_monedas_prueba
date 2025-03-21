f74b3175ffe7d92811827f3feba3ace79f133d9b
jiuraque <jiulianna.raquel@gmail.com>
2025-03-21 14:17:59 -0300

    se actualiza el script

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,30 +8,19 @@ const resultSpan = document.getElementById("result");
 const chartCanvas = document.getElementById("chart");
 let myChart = null;
 
-async function getExchangeRate(currency) {
-    try {
-        const response = await fetch(api);
-        if (!response.ok) {
-            throw new Error("Error al obtener los datos de la API");
-        }
-        const data = await response.json();
+// Capturar el valor del input
 
+function handleInput() {
+    const amount = parseFloat(amountInput.value);
 
-        if (currency === "dolar") return data.dolar.valor;
-        if (currency === "euros") return data.euro.valor;
-        if (currency === "pesos") return data["dolar"].valor * data["dolar_intercambio"].valor / data["euro"].valor;
-
-        return null;
-    } catch (error) {
-        console.error("Error al obtener las tasas de cambio:", error);
-        resultSpan.textContent = "Error al obtener datos.";
-        return null;
+    if (isNaN(amount) || amount <= 0) {
+        console.log("Ingrese un monto válido."); 
+    } else {
+        console.log("Monto ingresado en CLP:", amount); 
     }
-    
-    console.log(getExchangeRate);
 }
 
-
-
+// Evento Boton
+convertButton.addEventListener("click", handleInput);
 
 
